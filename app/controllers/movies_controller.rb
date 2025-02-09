b2721b94e5af449e259a6a431e133bce47adc06a
Saba-mt <saba@Saba>
2025-02-08 22:48:17 -0600

    Cleaned up and optimized movies.css

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,9 +1,11 @@
 class MoviesController < ApplicationController
-  before_action :set_movie, only: %i[ show edit update destroy ]
+  before_action :set_movie, only: %i[show edit update destroy]
 
   # GET /movies or /movies.json
   def index
-    @movies = Movie.all
+    sort_column = params[:sort] || "title"
+    sort_direction = params[:direction] || "asc"
+    @movies = Movie.order("#{sort_column} #{sort_direction}")
   end
 
   # GET /movies/1 or /movies/1.json
@@ -58,13 +60,14 @@ class MoviesController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_movie
-      @movie = Movie.find(params.expect(:id))
-    end
 
-    # Only allow a list of trusted parameters through.
-    def movie_params
-      params.expect(movie: [ :title, :rating, :description, :release_date ])
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_movie
+    @movie = Movie.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def movie_params
+    params.require(:movie).permit(:title, :rating, :description, :release_date)
+  end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,22 @@
 module ApplicationHelper
+  # Toggles sorting direction between ascending and descending
+  def toggle_direction(column, current_direction)
+    if column == params[:sort]
+      current_direction == "asc" ? "desc" : "asc"
+    else
+      "asc"
+    end
+  end
+
+  # CSS class for highlighting the sorted column
+  def sorted_column_class(column)
+    column == params[:sort] ? "sorted-#{params[:direction]}" : ""
+  end
+
+  # Sorting indicator (▲ for ascending, ▼ for descending)
+  def sort_indicator(column)
+    return "" unless column == params[:sort]
+
+    params[:direction] == "asc" ? "▲" : "▼"
+  end
 end
