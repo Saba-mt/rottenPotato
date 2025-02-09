f9cfd44f749688467bf458bd9771ab8dda080613
Saba-mt <saba@Saba>
2025-02-08 22:02:12 -0600

    Added sortable table and styles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def toggle_direction(column, current_direction)
+    if column == params[:sort]
+      current_direction == "asc" ? "desc" : "asc"
+    else
+      "asc"
+    end
+  end
 end
